0ae753859a2dc49a51711be3ed8af5825e47ff73
DeetSteve00 <98794456+DeetSteve00@users.noreply.github.com>
2025-10-16 12:37:25 -0600

    Actualizar KefaAndPark.cpp

--- PROBLEMS/KefaAndPark.cpp
+++ PROBLEMS/KefaAndPark.cpp
@@ -3,12 +3,11 @@
 using namespace std;
 
 vector<vector<int>> tree;
-vector<int> c, v;
+vector<int> c;
 int n, m, ans = 0;
 
-void dfs(int n, int d) {
-	if(v[n] || d > m) return;
-	v[n] = 1;
+void dfs(int n, int p, int d) {
+	if(d > m) return;
 
 	if(c[n]) d++;
 	else d = 0;
@@ -16,18 +15,16 @@ void dfs(int n, int d) {
 	if(tree[n].size() == 1 && d <= m && n != 0) ans++;
 
 	for(int k : tree[n])
-		dfs(k, d);
+		if(k != p) dfs(k, n, d);
 }
 
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
 	cin >> n >> m;
 
-	c.resize(n); v.resize(n);
-	for(int i = 0; i < n; i++) {
-		cin >> c[i];
-		v[i] = 0;
-	}
+	c.resize(n);
+	for(int& k : c)
+		cin >> k;
 
 	tree.resize(n);
 	for(int i = 1; i < n; i++) {
@@ -39,7 +36,7 @@ int main() {
 		tree[y].push_back(x);
 	}
 
-	dfs(0, 0);
+	dfs(0, -1, 0);
 	cout << ans << '\n';
 	return 0;
 }
